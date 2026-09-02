3ec77ec19963b21e60a8fc39e1a0e4cb46ba5af4
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-03 03:52:50 +0600

    Deleted task route and tested by postman

--- server/src/routes/task.routes.ts
+++ server/src/routes/task.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTask } from "../controllers/task.controller";
+import { createTask, deleteTask } from "../controllers/task.controller";
 import { auth } from "../middlewares/auth";
 
 const router = Router();
 
 router.post("/", auth, createTask);
+router.delete("/:id", auth, deleteTask);
 
 export default router;
