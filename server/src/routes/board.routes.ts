81e8611486c26b82878d662fb5d266ac4358ded3
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-03 03:12:51 +0600

    Shared board with other member route

--- server/src/routes/board.routes.ts
+++ server/src/routes/board.routes.ts
@@ -3,6 +3,7 @@ import {
   createBoard,
   getBoards,
   getBoardById,
+  shareBoard,
 } from "../controllers/board.controller";
 import { auth } from "../middlewares/auth";
 
@@ -11,5 +12,6 @@ const router = Router();
 router.post("/", auth, createBoard);
 router.get("/", auth, getBoards);
 router.get("/:id", auth, getBoardById);
+router.post("/:id/share", auth, shareBoard);
 
 export default router;
