2e2aa54a84d85346e2b3f0d3b6ef2d223f4faabe
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-03 02:04:06 +0600

    Setup bcrypt for password hashing

---
+++ server/src/utils/bcrypt.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcryptjs";
+
+export const hashPassword = async (password: string): Promise<string> => {
+  return await bcrypt.hash(password, 10);
+};
+
+export const comparePassword = async (
+  password: string,
+  hash: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hash);
+};
