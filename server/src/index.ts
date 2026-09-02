29be2bcf64a9edc11f8dac481075c1c3e7f5be3f
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-03 03:38:24 +0600

    Added column router in main page and tested by postman

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import { errorHandler } from "./middlewares/errorHandler";
 // Routes Import
 import authRoutes from "./routes/auth.routes";
 import boardRoutes from "./routes/board.routes";
+import columnRoutes from "./routes/column.routes";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/boards", boardRoutes);
+app.use("/api/columns", columnRoutes);
 
 // Health Check
 app.get("/api/health", (req, res) => {
