a6ddab437f5d22aea7e816eba5137adb4f537eb0
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-04 00:13:09 +0600

    Task routes

--- server/src/routes/task.routes.ts
+++ server/src/routes/task.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { createTask, deleteTask } from "../controllers/task.controller";
+import {
+  createTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  moveTask,
+  updateTask,
+} from "../controllers/task.controller";
 import { auth } from "../middlewares/auth";
 
 const router = Router();
 
 router.post("/", auth, createTask);
-router.delete("/:id", auth, deleteTask);
+router.get("/", auth, getTasks);
+router.get("/:taskId", auth, getTaskById);
+router.patch("/:taskId", auth, updateTask);
+router.delete("/:taskId", auth, deleteTask);
+router.put("/:taskId/move", auth, moveTask);
 
 export default router;
