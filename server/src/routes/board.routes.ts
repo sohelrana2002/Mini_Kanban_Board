3c89a3df748f2b6ad746ff3d732eb78cf29d23d8
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-03 21:24:13 +0600

    Remove board member route

--- server/src/routes/board.routes.ts
+++ server/src/routes/board.routes.ts
@@ -6,6 +6,7 @@ import {
   shareBoard,
   deleteBoard,
   updateBoardTitle,
+  removeBoardMember,
 } from "../controllers/board.controller";
 import { auth } from "../middlewares/auth";
 
@@ -17,5 +18,6 @@ router.get("/:id", auth, getBoardById);
 router.patch("/:boardId/title", auth, updateBoardTitle);
 router.post("/:id/share", auth, shareBoard);
 router.delete("/:id/delete", auth, deleteBoard);
+router.delete("/:boardId/members/:memberId", auth, removeBoardMember);
 
 export default router;
