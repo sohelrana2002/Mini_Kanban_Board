912fe9bb4fdd6f6eb883c1c5c55c4cdbe00d015c
SOHEL RANA <sohelrana104322@gmail.com>
2026-09-05 01:33:51 +0600

    Toast added for logout

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -93,6 +93,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
     setToken(null);
     setUser(null);
+    toast.success("Successfully log out");
     router.push("/login");
   }, [router]);
 
